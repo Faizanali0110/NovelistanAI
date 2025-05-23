52b9695413c7d73a0cd023db7c91a68df68229b2
Faizanali0101 <01-131222-017@bahria.edu.pk>
2025-05-23 17:19:46 +0500

    feat(frontend): add App component and styles

---
+++ novelistan/src/App.jsx
@@ -0,0 +1,33 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import NovelLandingPage from "./pages/NovelLandingPage";
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import AuthorHandling from './Author/AuthorHandling';
+import NotFoundPage from './NotFoundPage';
+import CustomerHandling from './Customer/CustomerHandling';
+import { ThemeProvider } from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+
+const App = () => {
+
+
+  return (
+    <ThemeProvider>
+      <ThemeToggle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<NovelLandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/AuthorHandling/*" element={<AuthorHandling />} />
+          <Route path="/CustomerHandling/*" element={<CustomerHandling />} />
+          <Route path="*" element={<NotFoundPage/>} /> {/* Fallback */}
+        </Routes>
+      </Router>
+    </ThemeProvider>);
+};
+
+export default App;
