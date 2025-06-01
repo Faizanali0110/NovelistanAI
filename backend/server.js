0cda8fa5b5c806cf2bbb45e83d48b0688333f0a7
Faizanali0110 <01-131222-017@student.bahria.edu.pk>
2025-06-01 11:15:11 +0500

    Update server.js to fix CORS for Azure backend deployment

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.use(cors({
       'https://novelistanai.azurewebsites.net', // Your Azure backend URL
       'http://localhost:3000',                  // Local development URLs
       'http://localhost:5173',
-      'https://novelistanai-backend-deployment-gkhae2hca5acf4b5.canadacentral-01.azurewebsites.net/'
+      'https://novelistanai-backend-deployment-gkhae2hca5acf4b5.canadacentral-01.azurewebsites.net'
     ];
     
     if (allowedOrigins.indexOf(origin) !== -1) {
